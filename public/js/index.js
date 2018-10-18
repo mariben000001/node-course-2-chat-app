98095c1bddbb4e110eb6201f1fea17715b9a9742
Illimitado Admin <you@example.com>
2018-10-18 14:42:53 +0800

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -8,31 +8,27 @@ console.log('Disconnected from Server');
 });
 //listen to event
 socket.on('newMessage', function(message) {
-console.log('newMessage', message);
-var formattedTime = moment(message.createdAt).format('h:mm a');
-var li = jQuery('<li></li>');
-li.text(`${message.from} ${formattedTime}: ${message.text}`);
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery('#message-template').html();
+	var html = Mustache.render(template, {
+	text: message.text,
+	from: message.from,
+	createdAt: formattedTime
+	});
+	jQuery('#messages').append(html);
 
-jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
-var li = jQuery('<li></li>');
-var a = jQuery('<a target = "_blank">My Current location</a>');//target another tab
-var formattedTime = moment(message.createdAt).format('h:mm a');
-
-li.text(`${message.from} ${formattedTime}: `);
-a.attr('href', message.url);
-li.append(a);
-jQuery('#messages').append(li);
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery('#location-message-template').html();
+	var html = Mustache.render(template, {
+	from: message.from,
+	url: message.url,
+	createdAt: formattedTime
+	});
+	jQuery('#messages').append(html);
 });
-/*
-socket.emit('createMessage', {
-from: 'Frank',
-text: 'Hi'
-}, function (data) {
-	console.log('Got it', data);
-});*/
 
 jQuery('#message-form').on('submit', function (e) {
 	e.preventDefault(); //prevent to go to URL the typed
